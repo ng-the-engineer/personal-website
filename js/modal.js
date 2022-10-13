c3d40b3d9b3f1ed50e8bfa2cb16175f5b96f4e11
Anthony Ng <engineer.anthony.ng@gmail.com>
2022-10-13 07:56:04 +0100

    Add Travie pdf

--- js/modal.js
+++ js/modal.js
@@ -3,9 +3,17 @@ const modalAutobid = document.getElementById("modalAutobid");
 document.getElementById("allPrjAutobid").onclick = () => modalAutobid.style.display = "block";
 document.getElementById("closeModalAutobid").onclick = () => modalAutobid.style.display = "none";
 
+// Travie
+const modalTravie = document.getElementById("modalTravie");
+document.getElementById("allPrjTravie").onclick = () => modalTravie.style.display = "block";
+// document.getElementById("travie_hist").onclick = () => modalTravie.style.display = "block";
+document.getElementById("closeModalTravie").onclick = () => modalTravie.style.display = "none";
+
 window.onclick = (event) => {
     switch(event.target) {
       case modalAutobid:
         modalAutobid.style.display = "none";
+      case modalTravie:
+        modalTravie.style.display = "none";
     }
   };
